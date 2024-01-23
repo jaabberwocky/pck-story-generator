f940519dc3709c2e206e64526fd41a0d2a190d2d
tobias <tobias@tobias-leong.dev>
2024-01-23 11:24:31 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Phua Chu Kang story generator
+Fun project to generate Phua Chu Kang-styled stories from image using multi-modal models with the Hugging Face `transformers` library, and stitching it up with prompt templates using `langchain`. Also took the opportunity to make a very simple web app using `streamlit` to serve the model.
+
+## Demo
+![demo](demo.png)
+
+## FAQ
+1. Why?
+    - Why not? 😊
+2. Why Phua Chu Kang?
+    - Why not? 😊
+3. Do you store the images
+    - No, no images are persisted on the backend. The bytes from the image are directly passed for model inference.
+
+## Credits
+PCK image from [here](https://www.big3.sg/blog/singapore-cultural-icon-phua-chu-kang).
---
+++ app.py
@@ -0,0 +1,72 @@
+from dotenv import load_dotenv, find_dotenv
+from transformers import pipeline
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+import streamlit as st
+from PIL import Image
+import io
+import datetime
+
+
+def load_config():
+    load_dotenv(find_dotenv())
+
+
+def img2text(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes))
+    image_to_text = pipeline(
+        'image-to-text', model='nlpconnect/vit-gpt2-image-captioning')
+    text = image_to_text(image)[0]['generated_text']
+    return text
+
+
+def text_to_story(scenario):
+    template = """
+    Imagine you are an expert storyteller with the voice of Phua Chua Kang from Singapore. In the local Singlish dialect, generate a story
+    of not more than 100 words.
+
+    CONTEXT: {scenario}
+    STORY:
+    """
+
+    prompt = PromptTemplate.from_template(template)
+    llm = ChatOpenAI()
+    output_parser = StrOutputParser()
+
+    chain = prompt | llm | output_parser
+    story = chain.invoke({'scenario': scenario})
+    return story
+
+
+def log_call(caption, story):
+    log_entry = f"Timestamp: {datetime.datetime.now()}|{caption}|{story}\n\n"
+    with open("log.txt", "a") as f:
+        f.write(log_entry)
+    print(log_entry)
+
+
+def draw_ui():
+    st.set_page_config(
+        page_title="PCK!", page_icon="🇸🇬")
+    st.header("Eh PCK can help you tell story one!")
+    st.image("phuachukang.jpg")
+    uploaded_file = st.file_uploader(
+        "Show me picture lah...", type=["jpg", "png"])
+
+    if uploaded_file is not None:
+        img_bytes = uploaded_file.read()
+        st.image(uploaded_file, caption='This one you send one right?',
+                 use_column_width=True)
+        st.write("")
+        with st.spinner("Wait ah, I thinking..."):
+            with st.expander("PCK says:", expanded=True):
+                caption = img2text(img_bytes)
+                story = text_to_story(caption)
+                log_call(caption, story)
+                st.write(story)
+
+
+if __name__ == "__main__":
+    load_config()
+    draw_ui()
